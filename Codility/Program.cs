d31ead6d853bd6233698b2e616561e3128776ecc
Hans Baldetorp <HaunsTM@users.noreply.github.com>
2021-05-06 21:42:34 +0200

    Added more test answers

--- Codility/Program.cs
+++ Codility/Program.cs
@@ -10,7 +10,6 @@ namespace Codility
             var _1_Iterations_BinaryGap_A_0 = 32;
             var _1_Iterations_BinaryGap_A_4 = 529;
             var _1_Iterations_BinaryGap_A_1 = 20;
-
             var _1_Iterations_BinaryGap = new _1_Iterations_BinaryGap();
             var _1_Iterations_BinaryGap_answer_5 = _1_Iterations_BinaryGap.solution(_1_Iterations_BinaryGap_A_5);
             var _1_Iterations_BinaryGap_answer_0 = _1_Iterations_BinaryGap.solution(_1_Iterations_BinaryGap_A_0);
@@ -18,9 +17,14 @@ namespace Codility
             var _1_Iterations_BinaryGap_answer_1 = _1_Iterations_BinaryGap.solution(_1_Iterations_BinaryGap_A_1);
             
             var _2_Arrays_OddOccurrencesInArray_A_7 = new int[] { 9, 3, 9, 3, 9, 7, 9 };
-
             var _2_Arrays_OddOccurrencesInArray = new _2_Arrays_OddOccurrencesInArray();
             var _2_Arrays_OddOccurrencesInArray_answer_7 = _2_Arrays_OddOccurrencesInArray.solution(_2_Arrays_OddOccurrencesInArray_A_7);
+
+            var _3_TimeComplexity_FrogJmp_X = 10;
+            var _3_TimeComplexity_FrogJmp_Y = 85;
+            var _3_TimeComplexity_FrogJmp_D = 30;
+            var _3_TimeComplexity_FrogJmp = new _3_TimeComplexity_FrogJmp();
+            var _3_TimeComplexity_FrogJmp_answer_3 = _3_TimeComplexity_FrogJmp.solution(_3_TimeComplexity_FrogJmp_X, _3_TimeComplexity_FrogJmp_Y, _3_TimeComplexity_FrogJmp_D);
 
             var _4_CountingElements_MissingInteger_A_5 = new int[] { 1, 3, 6, 4, 1, 2 };
             var _4_CountingElements_MissingInteger_A_4 = new int[] { 1, 2, 3 };
@@ -39,15 +43,26 @@ namespace Codility
             var _5_PrefixSums_CountDiv_answer_3 = _5_PrefixSums_CountDiv.solution(_5_PrefixSums_CountDiv_A, _5_PrefixSums_CountDiv_B, _5_PrefixSums_CountDiv_K);
 
             var _6_Sorting_Distinct_A_3 = new int[] { 2, 1, 1, 2, 3, 1 };
-
             var _6_Sorting_Distinct = new _6_Sorting_Distinct();
             var _6_Sorting_Distinct_answer_3 = _6_Sorting_Distinct.solution(_6_Sorting_Distinct_A_3);
 
+            var _7_StacksAndQueues_Nesting_S_0 = "())";
+            var _7_StacksAndQueues_Nesting_S_1 = "((as)(())())";
+            var _7_StacksAndQueues_Nesting = new _7_StacksAndQueues_Nesting();
+            var _7_StacksAndQueues_Nesting_answer_0 = _7_StacksAndQueues_Nesting.solution(_7_StacksAndQueues_Nesting_S_0);
+            var _7_StacksAndQueues_Nesting_answer_1 = _7_StacksAndQueues_Nesting.solution(_7_StacksAndQueues_Nesting_S_1);
 
             var _10_PrimeAndCompositeNumbers_CountFactors_8 = 2435514;
 
             var _10_PrimeAndCompositeNumbers_CountFactors = new _10_PrimeAndCompositeNumbers_CountFactors();
             var _10_PrimeAndCompositeNumbers_CountFactors_answer_8 = _10_PrimeAndCompositeNumbers_CountFactors.solution(_10_PrimeAndCompositeNumbers_CountFactors_8);
+
+           
+            var _90_TasksFromIndeedPrime2015Challenge_LongestPassword_S = "test 5 a0A pass007 ?xy1";
+
+            var _90_TasksFromIndeedPrime2015Challenge_LongestPassword = new _90_TasksFromIndeedPrime2015Challenge_LongestPassword();
+            var _90_TasksFromIndeedPrime2015Challenge_LongestPassword_answer_S = _90_TasksFromIndeedPrime2015Challenge_LongestPassword.solution(_90_TasksFromIndeedPrime2015Challenge_LongestPassword_S);
+
             int i = 0;
         }
     }
---
+++ Codility/_7_StacksAndQueues_Nesting.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility
+{
+    class _7_StacksAndQueues_Nesting
+    {   
+        private bool isCorrespondingParentheses(char nestCharacterStart, char nestCharacterEnd)
+        {
+            var isValid = false;
+
+            if (nestCharacterStart == '(' && nestCharacterEnd == ')')
+            {
+                isValid = true;
+            }
+
+            return isValid;
+        }
+
+        public int solution(string S)
+        {
+            const int IS_NOT_PROPERLY_NESTED = 0;
+            const int IS_PROPERLY_NESTED = 1;
+
+            var charStack = new Stack<char>();
+
+            //split input string to char array
+            var characterArray = S.ToCharArray();
+
+            foreach (var character in characterArray)
+            {
+                if (character == '(') 
+                {
+                    // we have found an opening parenthese, put it on the character stack
+                    charStack.Push(character);
+                } 
+                else if (character == ')')//need to check for 
+                {
+                    // if we do not have any character on the stack when we are here, it's impossible that the parentheses are properly corresponding
+                    if (charStack.Count == 0) 
+                        return IS_NOT_PROPERLY_NESTED; 
+
+                    // remove topmost character from stack 
+                    var topmostChar = charStack.Pop();
+                    var nestCharacterStart = topmostChar;
+                    var nestCharacterEnd = character;
+
+                    // verify corresponding pair of parentheses
+                    if (!isCorrespondingParentheses(nestCharacterStart, nestCharacterEnd))
+                    {
+                        return IS_NOT_PROPERLY_NESTED;
+                    }
+                }
+            }
+
+            // if we end up here, the character stack should be empty
+            if (charStack.Count != 0)
+                return IS_NOT_PROPERLY_NESTED;
+
+            return IS_PROPERLY_NESTED;
+        }
+    }
+}
